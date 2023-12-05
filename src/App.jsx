9db64a875571764c049fa53b1412624394ccee54
majorTom <t.schmidt97@gmx.de>
2023-12-05 11:51:38 +0100

    Zählbutton

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Content from "./Content.jsx";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import React from "react";
 
 function App() {
 	return (
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,11 +1,26 @@
+import React, { useState } from 'react';
+
 function Content() {
+	const [count, setCount] = useState(0);
+
+    const incrementCount = () => {
+        setCount(prevCount => prevCount + 1);
+		
+		console.log(count);
+    };
+
 	return (
 		<main className="flex-grow">
 			<div className="container mx-auto p-4">
 				<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-					<div className="bg-gray-300 p-4">Spalte 1</div>
+					<div className="bg-gray-300 p-4">{count}</div>
 					<div className="bg-gray-400 p-4">Spalte 2</div>
-					<div className="bg-gray-500 p-4">Spalte 3</div>
+					<div className="bg-gray-500 p-4">
+						<button 
+                      	  className="bg-gray-500 text-white p-2 rounded"
+                      	  onClick={incrementCount}
+                    	>Erhöhen</button>
+					</div>
 				</div>
 			</div>
 		</main>
